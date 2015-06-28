174a8eafa1b696304227c586430f5ca6c05fd74d
Sebas Sujeen <sreymondjohn@cs.stonybrook.edu>
2015-06-27 19:24:47 -0700

    Added mystery5.c No notes included because it is trivial

---
+++ mystery5.c
@@ -0,0 +1,13 @@
+char *mystery5(int x)
+{
+	if (x == 6)
+		return "E";
+	else if (x == 7)
+		return "D";
+	else if (x == 8)
+		return "C";
+	else if (x == 9)
+		return "B";
+	else 
+		return "A";
+}
